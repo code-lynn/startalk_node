941f47b1461c48157badee00fdfecf2ee41f6045
yuelin.liu <yuelin.liu@qunar.com>
2019-05-29 17:49:43 +0800

    web

--- node_config.js
+++ node_config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    "fileurl":"",
-    "javaurl":"",
-    "httpurl":"",
-    "apiurl":"",
-    "searchurl":"",
+    "fileurl":"http://100.80.129.58:8080",
+    "javaurl":"http://100.80.129.58:8080/package",
+    "httpurl":"http://100.80.129.58:8080/newapi",
+    "apiurl":"http://100.80.129.58:8080/api",
+    "searchurl":"http://100.80.129.58:8080/search/search.py",
     "web":{
         "title":'Startalk Web',
-        "stycss": '',
+        "stycss": 'index_1a6a9bbe.css',
         "scrcss": '',
-        "sdkjs": '',
-        "scrjs": ''
+        "sdkjs": 'qtalk_web_sdk@1.0.0.js',
+        "scrjs": 'index_ab77b76a.js'
     }
 }
